3a3e3201afc4c49ad8eb656af8b981876d5911bf
Hai Guo <guohaia1102640848@gmail.com>
2026-03-15 15:41:10 -0700

    Delete 515_streamlit directory

--- 515_streamlit/components/input_panel.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-
-def render():
-    movie = st.text_input("Movie Name")
-    actor = st.text_input("Next Actor Name")
-    submit = st.button("Submit Step")
-    return movie, actor, submit
